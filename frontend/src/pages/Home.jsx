a27cb7be97776d5bbc368fdb67e82d1e6b3831ee
Wei He <breehope@me.com>
2026-01-13 01:20:27 -0800

    1. Changed the way to show message according to types; 2. Refactored createAssignment()

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import api from '../api'
 import { ASSIGNMENT_TYPES } from '../constants';
-import '../styles/Form.css'
+import '../styles/Login.css'
 import '../styles/Home.css'
+import '../styles/Global.css'
 import Assignment from '../components/Assignment';
+// import { datetime } from 'datetime-js';
 
 
 function Home() {
@@ -15,7 +17,9 @@ function Home() {
     const [description, setDescription] = useState('');
     const [total_score, setTotal_score] = useState(0);
     const [files, setFiles] = useState([]);
-    const [due, setDue] = useState('');
+    const [due, setDue] = useState(null);
+    const [message, setMessage] = useState('');
+    const isError = false;
 
     useEffect(() => {
         getAssignments();
@@ -34,24 +38,35 @@ function Home() {
         api
             .delete(`/api/assignments/delete/${id}/`)
             .then((res) => {
-                if (res.status === 204) alert('Assignment deleted!')
-                else alert(`Failed to delete assignment: ${id}`)
+                if (res.status === 204) setMessage('Assignment deleted!')
+                else {isError = true; setMessage(`Failed to delete assignment: ${id}`)}
                 getAssignments(); // should remove using js removal 
             })
-            .catch((err) => alert(err));
+            .catch((err) => {isError = true; setMessage(`Error deleting assignment: ${err}`)});
     }
 
-    const createAssignment = (e) => {
-        e.preventDefault()
-        api
-            .post('/api/assignments/', { course_id, type, title, description, total_score, due, files })
-            .then((res) => {
-                if (res.status === 201) alert('Assignment created!')
-                else alert('Failed to create assignment!')        
-                getAssignments(); // should remove using js removal 
-            })
-            .catch((err) => alert(err));
-    }
+    const createAssignment = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await api.post("/api/assignments/", {
+                course_id,
+                type,
+                title,
+                description,
+                total_score: Number(total_score),
+                due: new Date(due + ":00").toISOString() || null,
+                // files
+            });
+
+            // alert("Assignment created!");
+            setMessage("Assignment created!")
+            getAssignments(); // refresh list
+        } catch (err) {
+            const msg = err.response?.data?.detail || "Failed to create assignment";
+            alert(msg);
+        }
+    };
 
     const loadCourses = () => {
         api
@@ -64,7 +79,7 @@ function Home() {
     return <div>
         <div>
             <h2>Assignments</h2>
-            { assignments.map((a) => <Assignment assignment={a} onDelete={deleteAssignment} key={a.id} />) }
+            {assignments.map((a) => <Assignment assignment={a} onDelete={deleteAssignment} key={a.id} />)}
         </div>
         <h2>Create an Assignment</h2>
         <form onSubmit={createAssignment}>
@@ -73,9 +88,9 @@ function Home() {
             <select id="course" value={course_id} onChange={(e) => setCourse_Id(e.target.value)}>
                 {/* <option value="">--Please choose a course--</option> */}
                 {allCourses.map((course) => (
-                <option key={course.id} value={course.id}>
-                    G{course.grade_level}-P{course.period}-{course.course_name}
-                </option>
+                    <option key={course.id} value={course.id}>
+                        G{course.grade_level}-P{course.period}-{course.course_name}
+                    </option>
                 ))}
             </select>
             <br />
@@ -84,67 +99,70 @@ function Home() {
             <select id="type" value={type} onChange={(e) => setType(e.target.value)}>
                 {/* <option value="">--</option> */}
                 {ASSIGNMENT_TYPES.map((a) => (
-                <option key={a.value} value={a.value}>
-                    {a.text}
-                </option>
+                    <option key={a.value} value={a.value}>
+                        {a.text}
+                    </option>
                 ))}
             </select>
             <br />
-            <label htmlFor='title'  className='required-field'>Title</label>
+            <label htmlFor='title' className='required-field'>Title</label>
             <br />
-            <input 
+            <input
                 className='form-input'
-                type='text' 
-                id='title' 
-                name='title' 
-                required 
-                onChange={(e) => setTitle(e.target.value)} 
+                type='text'
+                id='title'
+                name='title'
+                required
+                onChange={(e) => setTitle(e.target.value)}
             />
             <br />
-            <label htmlFor='description'  className='required-field'>Description</label>
+            <label htmlFor='description' className='required-field'>Description</label>
             <br />
-            <input 
+            <input
                 className='form-input'
-                type='text' 
-                id='description' 
-                name='description' 
-                required 
-                onChange={(e) => setDescription(e.target.value)} 
+                type='text'
+                id='description'
+                name='description'
+                required
+                onChange={(e) => setDescription(e.target.value)}
             />
             <br />
             <label htmlFor='total_score'>Total Score</label>
             <br />
-            <input 
+            <input
                 className='form-input'
-                type='text' 
-                id='total_score' 
-                name='total_score' 
-                required 
-                onChange={(e) => setTotal_score(e.target.value)} 
+                type='text'
+                id='total_score'
+                name='total_score'
+                required
+                onChange={(e) => setTotal_score(e.target.value)}
             />
             <br />
             <label htmlFor="due">Due</label>
             <br />
-            <input 
-                type="datetime-local" 
-                id="due" 
+            <input
+                type="datetime-local"
+                id="due"
                 name="due"
                 value={due}
-                onChange={(e) => setDue(e.target.value)} 
+                onChange={(e) => setDue(e.target.value)}
             />
             <br />
             <label htmlFor='files'>Files</label>
             <br />
-            <input 
+            <input
                 className='form-input'
-                type='text' 
-                id='files' 
-                name='files' 
-                onChange={(e) => setFiles(e.target.value)} 
+                type='text'
+                id='files'
+                name='files'
+                onChange={(e) => setFiles(e.target.value)}
             />
             <button className='form-button' type='submit'>
                 Create Assignment
             </button>
+            <span className={`message ${isError ? "error" : "success"}`}>
+                {message}
+            </span>
         </form>
     </div>
 }
