11ed1314191f7440ec3c70006fa1277401472f95
Wei He <breehope@me.com>
2026-01-13 01:13:16 -0800

    Avoided page reloading(which would lose JWT)

--- frontend/src/components/assignment.jsx
+++ frontend/src/components/assignment.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
 import '../styles/Assignment.css'
+import { Link } from "react-router-dom";
 
 function Assignment({assignment, onDelete}) {
     const formattedDate = new Date(assignment.created_at).toLocaleDateString('en-US')
     const formattedDue = new Date(assignment.due).toLocaleDateString('en-US')
 
     return <div className='assignment-container'>
-        <a 
-            href={`/assignment/${ assignment.id }/${ assignment.course_id }/students`} 
+        <Link 
+            to={`/assignment/submissions/`} 
+            state={{ assignment: assignment }}
             className='assignment-title' 
         >
                 {assignment.title}
-        </a>
+        </Link>
         <p className='assignment-date'>{formattedDate}</p>
         <p className='assignment-date'>{formattedDue}</p>
         <button className='delete-button' onClick={() => onDelete(assignment.id)}>
