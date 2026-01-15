d9b5f3c76c8a3e5e527b4bbee948ea6d8a4145a9
Wei He <breehope@me.com>
2026-01-14 22:49:23 -0800

    1. Modified token APIs to use cookie for refresh token 2. Corrected the way to get param from request for GetCourseStudents

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,12 +6,74 @@ from .serializers import (UserSerializer, CourseSerializer, AssignmentSerializer
     CourseStudentsSerializer, SubmissionSerializer, SubmissionDetailSerializer, SubmissionUpdateSerializer)
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Assignment, Course, Submission
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+from django.http import HttpResponse
+from django.urls import get_resolver
+
+class CookieTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        data = response.data
+
+        refresh_token = data.get("refresh")
+        # access_token = data.get("access")
+
+        # Remove refresh token from JSON response
+        response.data.pop("refresh", None)
+
+        # Set HttpOnly cookie
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,
+            secure=True,        # True in production (HTTPS)
+            samesite="None",    # Required for cross-site cookies
+            max_age=60 * 60 * 24 * 7,  # 7 days
+        )
+
+        return response
+
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get("refresh_token")
+
+        if refresh_token is None:
+            return Response({"detail": "Refresh token missing"}, status=400)
+
+        serializer = TokenRefreshSerializer(data={"refresh": refresh_token})
+        serializer.is_valid(raise_exception=True)
+
+        access_token = serializer.validated_data["access"]
+
+        return Response({"access": access_token})
+
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+class GetUserWithGroupsView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'username'
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        lookup_value = self.kwargs[self.lookup_field]
+        return queryset.get(**{self.lookup_field: lookup_value})
+
+
+    def get_queryset(self):
+        username = self.kwargs.get('username')
+        password = self.kwargs.get('password')
+        # user = User.objects.get(username=username, password=password).groups.all()
+        user = User.objects.filter(username=username, password=password).select_related('groups')
+        # submission = Submission.objects.filter(pk=pk).select_related('assignment_id', 'student_id')
+        # groups = User.objects.get(username=username).groups.all()
+        return user
 
 class AssignmentListCreate(generics.ListCreateAPIView):
     serializer_class = AssignmentSerializer
@@ -24,10 +86,15 @@ class AssignmentListCreate(generics.ListCreateAPIView):
     
     # Manually set created_by as current user, since the model set it as read-only
     def perform_create(self, serializer):
-        if serializer.is_valid():
-            serializer.save(created_by=self.request.user)
-        else:
-            print(serializer.errors)
+        serializer.save(created_by=self.request.user)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            print("VALIDATION ERRORS:", serializer.errors)
+            return Response(serializer.errors, status=400)
+        return super().create(request, *args, **kwargs)
+
 
 class AssignmentDelete(generics.DestroyAPIView):
     serializer_class = AssignmentSerializer
@@ -48,8 +115,8 @@ class GetCourseStudents(generics.ListAPIView):
     serializer_class = CourseStudentsSerializer
 
     def get_queryset(self):
-        courseId = self.request.get_full_path().split('/')[-2]
-        course = Course.objects.get(id=UUID(courseId))
+        course_id = self.kwargs["course_id"]
+        course = Course.objects.get(id=course_id)
         courseStudents = course.student_set.all()
         
         return courseStudents
