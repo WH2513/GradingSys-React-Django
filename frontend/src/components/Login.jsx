4aa197eb25b6c1170544b2cbc3f259ef5a31a2a6
Wei He <breehope@me.com>
2026-01-14 22:53:45 -0800

    Renamed component from Form to Login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,42 +2,43 @@ import { useState } from 'react'
 import api from '../api'
 import { useNavigate } from 'react-router-dom'
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants'
-import '../styles/Form.css'
+import '../styles/Login.css'
+import '../styles/Global.css'
 import LoadingIndicator from './LoadingIndicator'
+import { useAuth } from '../context/AuthContext'
 
-function Form({ route, method }) {
+function Form({ route, method }) { // login form 
+    const { login } = useAuth();
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [loading, setLoading] = useState(false)
+    const [message, setMessage] = useState('')
+    let isError = false
     const navigate = useNavigate()
 
     const pageName = method === 'login' ? 'Login' : 'Register'
 
     const handleSubmit = async (e) => {
-        setLoading(true);
         e.preventDefault(); // prevent from reloading the page, which is the default behavior after submit
+        setLoading(true);
 
         try {
-            const res = await api.post(route, {username, password});
-            let naviTo = '/';
-            if (username.startsWith("test")) {
-                naviTo = '/student';
-            }
-            // const userRole = await api.post('/api/user/role/', {username, password});
-            // if (userRole.data.role == 'Teacher') {
-            //     naviTo = '/';
-            // } else {
-            //     naviTo = '/student';
-            // }
+            const res = await api.post(route, { username, password });
+            let naviTo = username.startsWith("test") ? "/student" : "/";
+
             if (method === 'login') {
-                localStorage.setItem(ACCESS_TOKEN, res.data.access);
-                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
-                navigate(naviTo)
+                login(res.data.access); // set the access token in context
+                navigate(naviTo);
             } else {
-                navigate('/login')
+                navigate('/login');
             }
-        } catch(error) {
-            alert(error)
+        } catch (error) {
+            isError = true;
+            if (error.message.includes('401')) {
+                setMessage("Invalid username or password. Please try again.");
+            } else {
+                setMessage("An error occurred. Please try again.\nError: " + error.message);
+            }
         } finally {
             setLoading(false);
         }
@@ -51,19 +52,24 @@ function Form({ route, method }) {
                 type='text'
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                placehoder='Username'
+                placeholder='Username'
+                required
             />
             <input
                 className='form-input'
                 type='password'
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                placehoder='Password'
+                placeholder='Password'
+                required
             />
-            { loading && <LoadingIndicator />}
+            {loading && <LoadingIndicator />}
             <button className='form-button' type='submit'>
                 {pageName}
             </button>
+            <span className={`message ${isError ? "error" : "success"}`}>
+                {message}
+            </span>
         </form>
     );
 }
